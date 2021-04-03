4ecacf98b54e54fdfee63a97929b657ac47a1f46
David O'Quigley <oquigld2@gmail.com>
2021-04-03 20:07:19 +0100

    Add and tested event listeners on cards.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
-function startGame(num1) {
+//Check for DOM to finish loading, then set game level to easy by default.
+
+if(document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", levelSelect(4));
+} else {
+	levelSelect(4);
+}
+
+function startGame() {
+	let cards = Array.from(document.getElementsByClassName("card-wrapper"));
+
+	cards.forEach(card => {
+		card.addEventListener("click", function() {
+			//game play...
+			console.log("click");
+		});
+	});
+}
+
+function levelSelect(num1) {
 
 	//To add if statement to check if the difficulty level selected is already the level
 	//being displayed, use .preventDefualt() else resetGame().
@@ -8,7 +27,7 @@ function startGame(num1) {
 	resetGame();
 
 	let totalPairs = (num1 * num1) / 2; //calculate the required number of divs
-	let gridBox = "";
+	let gridBox = "";	//create an empty string to hold the generated html
 
 	for (let i = 0; i < totalPairs; i++) {
 		for (let j = 0; j < 2; j++) {
@@ -28,16 +47,20 @@ function startGame(num1) {
 	$(".game-wrapper").css("display", "grid");
 	$(".game-wrapper").append(gridBox);
 
+	startGame();
+
 
 }
 
 
 function resetGame() {
 
-	//To add modal here to check if user is sure they want to reset the game.
+	//To add: modal here to check if user is sure they want to reset the game.
+	//To add: check what level is currently being played and match it.
+	$(".card-wrapper").remove();		//remove any previously generated game instances
+	$(".game-info").css("display", "none");
 
-	$(".card-wrapper").remove();
-	$(".game-wrapper").css("display", "none");
+	console.log('game reset');
 
 }
 
