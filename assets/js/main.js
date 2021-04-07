f4bcf51457458ea1db58a4fe7b33b4ad9b010984
David O'Quigley <oquigld2@gmail.com>
2021-04-07 15:44:58 +0100

    Fix reset by way of new function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-//controll z
+
 let firstCard, secondCard;
 let isCardFlipped = false;
 let gameBusy = false;
@@ -11,6 +11,8 @@ let matchedPairs = 0;
 let totalPairs;
 let endTime;
 let totalMoves;
+let timeLimit;
+let finalScore;
 
 //Check for DOM to finish loading, then set game level to easy by default.
 if (document.readyState === "loading") {
@@ -19,6 +21,7 @@ if (document.readyState === "loading") {
 	levelSelect(4, 100);
 }
 
+//--------------------------------------Start Game
 function startGame(time) {
 	resetTimer();
 	startTimer(time);
@@ -32,7 +35,7 @@ function startGame(time) {
 }
 
 
-//Fisher-Yates Shuffle Method
+//--------------------------------------Fisher-Yates Shuffle Method
 function shuffleCards(cards) {
 
 	for (let i = cards.length - 1; i > 0; i--) {
@@ -43,13 +46,10 @@ function shuffleCards(cards) {
 }
 
 //--------------------------------------Timer function
-
 function startTimer(time) {
-	console.log("timer running");
 	countdownTimer = setInterval(() => {
 		let newTime = time--;
 		$("#countdown").text(newTime);
-
 		if (newTime === 0) {
 			resetTimer();
 			gameOver();
@@ -57,20 +57,21 @@ function startTimer(time) {
 	}, 1000);
 }
 
+//--------------------------------------Reset Timer
 function resetTimer() {
 	// game over when timer runs out
 	clearInterval(countdownTimer);
 }
 
+//--------------------------------------Game Over
 function gameOver() {
 	console.log("game over");
 }
 
+//--------------------------------------Flip Card
 function flipCard(card) {
 
 	if (canFlipCard(card)) {
-		console.log('can flip');
-
 		//increment #moves-total by one each time
 		flipCounter();
 		card.classList.add("flipped");
@@ -98,11 +99,10 @@ function flipCard(card) {
 	}
 }
 
+//--------------------------------------Check to see when game is over
 function checkGameWin() {
 	// check to see when game is over
 	if (matchedPairs <= totalPairs - 2) {
-		console.log(matchedPairs + " of " + totalPairs);
-
 	} else {
 		victory();
 		// flip remainin pair of cards
@@ -114,13 +114,21 @@ function checkGameWin() {
 	}
 }
 
+//--------------------------------------Victory
 function victory() {
-
+	//Display You win overlay
 	endTime = $("#countdown").text();
 	totalMoves = $("#moves-total").text();
+	//calculate final score
+	let elapsedTime = totalTime - endTime; 
+	console.log(elapsedTime);
+	finalScore = Math.floor((elapsedTime / totalMoves) * 100);
+	console.log("your score is " + finalScore);
+
 	
 }
 
+//--------------------------------------Reset Cards (unflip)
 function resetCards() {
 	// it's not a match sound?
 	setTimeout(() => {
@@ -131,6 +139,7 @@ function resetCards() {
 	return true;
 }
 
+//--------------------------------------Move Counter
 function flipCounter() {
 
 	let clicks = $("#moves-total").text();
@@ -148,21 +157,24 @@ function cardMatchCheck(card1, card2) {
 	}
 }
 
+//--------------------------------------Prevent click during animations and on already matched cards
 function canFlipCard(card) {
 	// returns false if the current card is already flipped i.e. has 'flipped' class
 	return !card.classList.contains("flipped") && !gameBusy;
 
 }
 
+//--------------------------------------Level Select
 function levelSelect(num1, time) {
 
 	currentLevel = num1;
+	timeLimit = time;
 	matchedPairs = 0;
 
 	//To add if statement to check if the difficulty level selected is already the level
 	//being displayed, use .preventDefualt() else resetGame().
 
-	resetGame();
+	clearGameBoard();
 
 	totalPairs = (num1 * num1) / 2; //calculate the required number of divs
 	let gridBox = "";	//create an empty string to hold the generated html
@@ -189,28 +201,24 @@ function levelSelect(num1, time) {
 	$(".game-wrapper").css("display", "grid");
 	$(".game-wrapper").append(gridBox);
 
-	// if(num1 == 6) {
-	// 	this.classList.add(".card-wrapper.small");
-	// 	console.log("small");
-	// }
 	cards = Array.from(document.getElementsByClassName("card-wrapper"));
-	// deck = Array.from(document.getElementsByClassName("card-wrapper"));
 	shuffleCards(cards);
 	startGame(time);
 
-
-
 }
 
-
+//--------------------------------------Reset Game
 function resetGame() {
+	levelSelect(currentLevel, timeLimit);
+}
+
+//--------------------------------------Remove previously generated card divs
+function clearGameBoard() {
 
 	//To add: modal here to check if user is sure they want to reset the game.
 	//To add: check what level is currently being played and match it.
 	$(".card-wrapper").remove();		//remove any previously generated game instances
 	$("#moves-total").text(0);
-	console.log('game reset');
-
 }
 
 //To add check gameplay function to see what game is being played and adjust the time remaining
